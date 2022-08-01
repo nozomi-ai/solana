85bf3cbc5be44c6b0644619b714b13f607cf27e4
Harshal0902 <harshalraikwar07@gmail.com>
2022-08-02 02:14:21 +0530

    ✨ Able to add multiple pointers

--- explorer/src/components/RPCNodeMap.tsx
+++ explorer/src/components/RPCNodeMap.tsx
@@ -1,25 +1,58 @@
-import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
-import Map  from 'react-map-gl';
-import { MapView, FirstPersonView } from '@deck.gl/core';
-import {mapStyle} from "../utils/mapStyle";
+import React from 'react';
+import DeckGL from '@deck.gl/react/typed';
+import { StaticMap } from 'react-map-gl';
+import { mapStyle } from "../utils/mapStyle";
+import { ArcLayer } from '@deck.gl/layers';
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoieWFzaG4iLCJhIjoiY2tybjZrYjViMWg3ejMxbGltc25vNTB1NyJ9.jmSJX_oaS7BY8GDizT37EA';
-// pk.eyJ1IjoieWFzaG4iLCJhIjoiY2tybjZrYjViMWg3ejMxbGltc25vNTB1NyJ9.jmSJX_oaS7BY8GDizT37EA
-export function RPCNodeMap() {
-  const INITIAL_VIEW_STATE = {
-    longitude: 12.4,
-    latitude: 38.78,
-    zoom: 0.5,
-  };
-  const data = [
-    {sourcePosition: -122.41669, targetPosition: -122.41669}
-  ];
-  const layers = [
-    new LineLayer({id: 'line-layer', data})
-  ];
-  const MAP_STYLE= mapStyle;
-    // 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
+
+const DATA_URL = "./data/newValidators.json";
+
+const INITIAL_VIEW_STATE = {
+  longitude: 26.0063319,
+  latitude: 32.724422,
+  zoom: 1,
+  pitch: 0,
+  bearing: 0,
+  dragRotate: false
+};
+
+// const INITIAL_VIEW_STATE = ({
+//   initialViewState: {
+//     longitude: 26.0063319,
+//     latitude: 32.724422,
+//     zoom: 1,
+//     pitch: 0,
+//     bearing: 0
+//   },
+//   controller: true
+// });
+
+export function RPCNodeMap({
+  data = DATA_URL,
+}) {
+
+  const layer = new ArcLayer({
+    id: 'arc-layer',
+    data,
+    pickable: true,
+    opacity: 1,
+    getWidth: 2,
+    wrapLongitude: true,
+    getSourcePosition: (d) => [
+      parseFloat(d.from.longitude),
+      parseFloat(d.from.latitude)
+    ],
+    getTargetPosition: (d) => [
+      parseFloat(d.to.longitude),
+      parseFloat(d.to.latitude)
+    ],
+    getSourceColor: (d) => [Math.sqrt(d.fare_amount), 140, 0],
+    getTargetColor: (d) => [Math.sqrt(d.fare_amount), 140, 0],
+  });
+
+  const MAP_STYLE = mapStyle;
+
   return (
     <div className="card">
       <div className="card-header">
@@ -30,21 +63,14 @@ export function RPCNodeMap() {
       </div>
       <div className="card-body">
         <div className="map-card">
-        <DeckGL
+          <DeckGL
             initialViewState={INITIAL_VIEW_STATE}
-            // controller={true}
-            layers={layers}
-       >
-          <Map
-            reuseMaps
-            mapStyle= {MAP_STYLE}
-            mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} 
-            preventStyleDiffing={true}
-            />
-       </DeckGL>
-      </div>
-     
+            layers={[layer]}>
+            <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} mapStyle={MAP_STYLE} />
+          </DeckGL>
+        </div>
+
       </div>
     </div>
-  )
+  );
 }
