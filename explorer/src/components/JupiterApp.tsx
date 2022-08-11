27e5eae4bf0a048d76acc2688a60eb236e8c7f41
yash621 <Yash.Goel@propertyloop.co.uk>
2022-08-11 23:42:47 +0530

    integrated network checks

--- explorer/src/components/JupiterApp.tsx
+++ explorer/src/components/JupiterApp.tsx
@@ -1,11 +1,22 @@
 import React, { FC, ReactNode } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { JupiterProvider } from '@jup-ag/react-hook';
-import { Connection } from "@solana/web3.js";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+import { Cluster, clusterUrl, useCluster } from 'src/providers/cluster';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 export const JupiterApp: FC<{ children: ReactNode }> = ({ children }) => {
-    const url = "https://ssc-dao.genesysgo.net/";
+    const { cluster, customUrl } = useCluster();
+    const currentClusterUrl = clusterUrl(cluster, customUrl);
+    var url = "https://ssc-dao.genesysgo.net/";
+    if (cluster === Cluster.MainnetBeta) {
+        url = clusterApiUrl("mainnet-beta");
+    } else if (cluster === Cluster.Testnet) { 
+        url = clusterApiUrl("testnet");
+    } else {
+        url = clusterApiUrl("devnet");
+    }
+
     const connection = new Connection(url);
     const wallet = useWallet();
     return (
